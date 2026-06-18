434909c18613863c6e8acecdf2b3a5743f9bee11
SilmaraSantospereira <santos.silmara0@gmail.com>
2026-06-17 22:11:41 -0300

    Implement battle logic in Jogo class

--- src/main/java/br/edu/poo/Jogo.java
+++ src/main/java/br/edu/poo/Jogo.java
@@ -1,11 +1,37 @@
 package br.edu.poo;
 
 public class Jogo {
-
     public static void main(String[] args) {
+        System.out.println("=== BATALHA INICIADA ===\n");
 
-        // Implemente a batalha aqui
+        Personagem artemis = new Personagem("Artemis", 35, 12, 6);
+        Personagem goblin = new Personagem("Goblin", 20, 9, 4);
+        Pocao pocaoPequena = new Pocao("Poção Pequena", 10);
 
+        boolean goblinUsouPocao = false;
+
+        while (artemis.estaVivo() && goblin.estaVivo()) {
+            
+            artemis.atacar(goblin);
+
+            if (!goblin.estaVivo()) {
+                break; 
+            }
+
+            if (goblin.getVida() <= 14 && !goblinUsouPocao) {
+                goblin.usarPocao(pocaoPequena);
+                goblinUsouPocao = true;
+            } else {
+                goblin.atacar(artemis);
+            }
+        }
+
+        System.out.println("=== FIM DA BATALHA ===\n");
+
+        if (artemis.estaVivo()) {
+            System.out.println("Vencedor: " + artemis.getNome());
+        } else {
+            System.out.println("Vencedor: " + goblin.getNome());
+        }
     }
-
 }
