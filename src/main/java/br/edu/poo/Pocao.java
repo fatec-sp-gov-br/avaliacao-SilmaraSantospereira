c043058e535186fc7713a3fd61dc3f765ad2b795
SilmaraSantospereira <santos.silmara0@gmail.com>
2026-06-17 22:21:19 -0300

    Create Pocao class with name and healing attributes

---
+++ src/main/java/br/edu/poo/Pocao.java
@@ -0,0 +1,19 @@
+package br.edu.poo;
+
+public class Pocao {
+    private String nome;
+    private int cura;
+
+    public Pocao(String nome, int cura) {
+        this.nome = nome;
+        this.cura = cura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCura() {
+        return cura;
+    }
+}
