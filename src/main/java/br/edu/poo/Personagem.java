40a93ec6325a13ce9e39363b64b14cb2e9c3f24a
SilmaraSantospereira <santos.silmara0@gmail.com>
2026-06-17 22:10:53 -0300

    Refactor Pocao class into Personagem with combat methods

--- src/main/java/br/edu/poo/Personagem.java
+++ src/main/java/br/edu/poo/Personagem.java
@@ -1,19 +1,59 @@
 package br.edu.poo;
 
-public class Pocao {
+public class Personagem {
     private String nome;
-    private int cura;
+    private int vida;
+    private int ataque;
+    private int defesa;
+    private boolean pocaoUtilizada;
 
-    public Pocao(String nome, int cura) {
+    public Personagem(String nome, int vida, int ataque, int defesa) {
         this.nome = nome;
-        this.cura = cura;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.pocaoUtilizada = false;
+    }
+
+    public void atacar(Personagem inimigo) {
+        int dano = this.ataque - inimigo.defesa;
+        
+        if (dano < 1) {
+            dano = 1;
+        }
+        
+        System.out.println(this.nome + " ataca " + inimigo.getNome());
+        inimigo.receberDano(dano);
+        System.out.println(inimigo.getNome() + " possui " + inimigo.getVida() + " de vida\n");
+    }
+
+    public void receberDano(int dano) {
+        this.vida -= dano;
+        
+        if (this.vida < 0) {
+            this.vida = 0;
+        }
+    }
+
+    public void usarPocao(Pocao pocao) {
+        if (!this.pocaoUtilizada) {
+            this.vida += pocao.getCura();
+            this.pocaoUtilizada = true;
+            
+            System.out.println(this.nome + " usa " + pocao.getNome());
+            System.out.println(this.nome + " possui " + this.vida + " de vida\n");
+        }
+    }
+
+    public boolean estaVivo() {
+        return this.vida > 0;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public int getCura() {
-        return cura;
+    public int getVida() {
+        return vida;
     }
 }
