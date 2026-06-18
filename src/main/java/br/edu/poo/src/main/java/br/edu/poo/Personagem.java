68b1a787db29eb33080b75924c341b0d71194a6b
SilmaraSantospereira <santos.silmara0@gmail.com>
2026-06-17 22:22:24 -0300

    Add Personagem class with combat functionality

---
+++ src/main/java/br/edu/poo/src/main/java/br/edu/poo/Personagem.java
@@ -0,0 +1,51 @@
+package br.edu.poo;
+
+public class Personagem {
+    private String nome;
+    private int vida;
+    private int ataque;
+    private int defesa;
+    private boolean pocaoUtilizada;
+
+    public Personagem(String nome, int vida, int ataque, int defesa) {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.pocaoUtilizada = false;
+    }
+
+    public void atacar(Personagem inimigo) {
+        int dano = this.ataque - inimigo.defesa;
+        if (dano < 1) {
+            dano = 1;
+        }
+        inimigo.receberDano(dano);
+    }
+
+    public void receberDano(int dano) {
+        this.vida -= dano;
+        if (this.vida < 0) {
+            this.vida = 0;
+        }
+    }
+
+    public void usarPocao(Pocao pocao) {
+        if (!this.pocaoUtilizada) {
+            this.vida += pocao.getCura();
+            this.pocaoUtilizada = true;
+        }
+    }
+
+    public boolean estaVivo() {
+        return this.vida > 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+}
